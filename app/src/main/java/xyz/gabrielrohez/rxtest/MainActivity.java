b90a59a1257dae4cbdd444e406a250d146c3943c
Gabo Rohez <gabow95k@gmail.com>
2020-04-21 13:08:49 -0500

    added view binding and create second activity

--- app/src/main/java/xyz/gabrielrohez/rxtest/MainActivity.java
+++ app/src/main/java/xyz/gabrielrohez/rxtest/MainActivity.java
@@ -2,13 +2,32 @@ package xyz.gabrielrohez.rxtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import xyz.gabrielrohez.rxtest.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        setUpEvents();
+    }
+
+    private void setUpEvents() {
+        binding.btnRxRetrofit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RxRetrofitActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/xyz/gabrielrohez/rxtest/RxRetrofitActivity.java
@@ -0,0 +1,14 @@
+package xyz.gabrielrohez.rxtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RxRetrofitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rx_retrofit);
+    }
+}
